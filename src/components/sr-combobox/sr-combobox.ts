0e2e47f75edca764a67ba71a1eda40b67537ad66
SrMouraSilva <mateus.moura@hotmail.com>
2016-12-27 15:16:41 -0300

    Fix Pedalboard effects page: sr-combobox

--- src/components/sr-combobox/sr-combobox.ts
+++ src/components/sr-combobox/sr-combobox.ts
@@ -1,4 +1,5 @@
 import {Input, Output, EventEmitter, Component} from '@angular/core';
+import {Lv2Param} from '../../plugins-manager/model/lv2/lv2-param';
 
 
 @Component({
@@ -6,50 +7,38 @@ import {Input, Output, EventEmitter, Component} from '@angular/core';
   templateUrl: 'sr-combobox.html'
 })
 export class SrCombobox {
-  @Input() parameter : any;
+  @Input() parameter : Lv2Param;
   @Input() selected : any;
   @Output('onChange') onChange = new EventEmitter();
 
-  constructor() {
-    this.parameter = {
-      name: 'Combo',
-      scalePoints: [
-        { value: '4x12', label: '4x12' },
-        { value: '2x10', label: '2x10' },
-        { value: '1x15', label: '1x15' },
-        { value: '4x8', label: '4x8' }
-      ]
-    };
-    this.selected = '1x15';
-  }
 
   public get options() {
     return {
-      title: this.parameter.name,
+      title: this.parameter.data.name,
     };
   }
 
   public before() {
     let index = this.indexOf(this.selected) - 1;
     if (index == -1)
-      index = this.parameter.scalePoints.length - 1;
+      index = this.parameter.data.scalePoints.length - 1;
 
-    this.selected = this.parameter.scalePoints[index].value;
+    this.selected = this.parameter.data.scalePoints[index].value;
     this.update();
   }
 
   public next() {
     let index = this.indexOf(this.selected) + 1;
-    if (index == this.parameter.scalePoints.length)
+    if (index == this.parameter.data.scalePoints.length)
       index = 0;
 
-    this.selected = this.parameter.scalePoints[index].value;
+    this.selected = this.parameter.data.scalePoints[index].value;
     this.update();
   }
 
   private indexOf(value : string) : number {
     let index = 0;
-    for (let option of this.parameter.scalePoints) {
+    for (let option of this.parameter.data.scalePoints) {
       if (option["value"] == value)
         return index;
       index++;
