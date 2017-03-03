48cddfd6a1cda21f63279d499c255914d1958c61
SrMouraSilva <mateus.moura@hotmail.com>
2017-03-03 00:24:07 -0300

    Add option to hide connections in PedalboardManagePage

--- src/pages/pedalboard-manager/pedalboard-manager.ts
+++ src/pages/pedalboard-manager/pedalboard-manager.ts
@@ -1,8 +1,9 @@
 import { Component, ViewChild } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { Content, Platform, NavController, NavParams } from 'ionic-angular';
 
 import {PedalboardDrawerPage} from '../pedalboard-drawer/pedalboard-drawer';
 import {PluginsPage} from '../plugins/plugins';
+
 
 
 @Component({
@@ -10,18 +11,38 @@ import {PluginsPage} from '../plugins/plugins';
   templateUrl: 'pedalboard-manager.html'
 })
 export class PedalboardManagerPage {
+  @ViewChild(PluginsPage) pluginsPage : PluginsPage;
+
   @ViewChild(PedalboardDrawerPage) pedalboardDrawer : PedalboardDrawerPage;
   //@ViewChild(SrPedalboard) pedalboardElement : SrPedalboard;
-  @ViewChild(PluginsPage) pluginsPage : PluginsPage;
+  @ViewChild('splitPane') ionContent : Content;
 
   public showDrawer : boolean = true;
   public showPlugins : boolean = false;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  public split = false;
+  private mediaQuerie : MediaQueryList
+  private mediaQuerieCallback : (query: MediaQueryList) => void;
+
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private platform: Platform) {
+    this.mediaQuerieCallback = query => this.updateSplit(query);
+  }
 
   ionViewDidLoad() {
+    const WIDTH = '(max-width: 992px)';
+    this.mediaQuerie = this.platform.win().matchMedia(WIDTH);
+    this.mediaQuerie.addListener(this.mediaQuerieCallback);
+    this.mediaQuerieCallback(this.mediaQuerie)
+
     this.pedalboardDrawer.ionViewDidLoad();
     this.pluginsPage.ionViewDidLoad();
+  }
+
+  ionViewWillLeave() {
+    this.mediaQuerie.removeListener(this.mediaQuerieCallback);
   }
 
   toggleDrawer() {
@@ -30,5 +51,15 @@ export class PedalboardManagerPage {
 
   goToPlugins() {
     this.showPlugins = true;
+  }
+
+  private updateSplit(query : MediaQueryList) {
+    let element = this.ionContent.getNativeElement();
+
+    this.split = !query.matches;
+    if (!query.matches)
+      element.classList.add('split');
+    else
+      element.classList.remove('split');
   }
 }
